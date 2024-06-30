4944e3c8abd4b891210b1527dacf8dc2dd78503d
Durim Miziraj <durim.miziraj@gmail.com>
2024-06-30 14:50:31 +0200

    first commit

---
+++ client/Program.cs
@@ -0,0 +1,75 @@
+﻿/**
+* Explanation of the program:
+* This is a simple chat client that connects to a server.
+* It sends a message to the server and then reads a message from the server.
+* It then prints the message received from the server.
+*/
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client;
+/**
+* Class to represent a chat client.
+* It has a main method, and everything is static due to that.
+* Most other things are ready to convert to an object type of its own.
+* You need remove the static specifiers and add a constructor to do so.
+*/
+class ChatClient
+{
+    private static string server = "127.0.0.1";
+    private static int port = 13000;
+    private static string msg = string.Empty;
+/**
+* Main method to run the client.
+* NOTE: Remove this and replace it with a constructor.
+*/
+    static void Main()
+    {
+        using TcpClient client = new TcpClient(server, port);
+        using NetworkStream stream = client.GetStream();
+
+        while (true)
+        {
+            Console.WriteLine("Enter a message to send to the server: ");
+            msg = Console.ReadLine();
+
+            WriteBytesToStream(stream, msg!);
+
+            Console.WriteLine(
+                "Received: {0}",
+                ReadBytesFromStream(stream)
+            );
+        }
+    }
+/**
+* Method to write Bytes to a stream.
+* @param stream The stream to write to.
+* @param message The message to write to the stream.
+*/
+    private static void WriteBytesToStream(NetworkStream stream, string message)
+    {
+        byte[] ASCIIBytes = Encoding.ASCII.GetBytes(message);
+
+        stream.Write(
+            buffer: ASCIIBytes,
+            offset: 0,
+            count:  ASCIIBytes.Length
+        );
+    } 
+/**
+* Method to read Bytes from a stream.
+* @param stream The stream to read from.
+* @return The Bytes read from the stream.
+*/
+    private static string ReadBytesFromStream(NetworkStream stream)
+    {
+        byte[] bytes = new byte[256];
+
+        return Encoding.ASCII.GetString(
+            bytes: bytes,
+            index: 0,
+            count: stream.Read(bytes, 0, bytes.Length)
+        );
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,78 @@
+﻿/**
+* This is a simple chat server that listens for a client to connect to it.
+* Once a client connects, the server will wait for the client to send a message.
+* The server will then convert the message to uppercase and send it back to the client.
+* The server will continue to wait for messages from the client until the client sends an empty message.
+* The server will then close the connection to the client and stop listening for new connections.
+*/
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server;
+/**
+* The ChatServer class is the main class for the server.
+*/
+class ChatServer
+{
+    private static int port = 13000;
+    private static IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+    static void Main()
+    {
+        using TcpListener server = new TcpListener(localAddr, port);
+        server.Start();
+        Console.WriteLine("Server started");
+
+        Console.WriteLine("Waiting...");
+        using TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Connected to client");
+
+        using NetworkStream stream = client.GetStream();
+        Console.WriteLine("Waiting for data from client");
+        string? msg = string.Empty;
+
+        while (true)
+        {
+            // Fills the stream and the variable with data if data is supplied by the client
+            msg = ReadBytesFromStream(stream).ToUpper();
+            Console.WriteLine("Received data from client: " + msg);
+
+            if (string.IsNullOrEmpty(msg)) { break; }
+
+            // Empties the stream
+            WriteBytesToStream(stream, msg);
+            Console.WriteLine("Sent data to client");
+        }
+    }
+/**
+* Reads bytes from the stream and converts them to a string.
+* @param stream The stream to read from.
+* @return The string representation of the bytes read from the stream.
+*/
+    private static string ReadBytesFromStream(NetworkStream stream)
+    {
+        byte[] bytes = new byte[256];
+
+        return Encoding.ASCII.GetString(
+            bytes,
+            0,
+            stream.Read(bytes, 0, bytes.Length)
+        );
+    }
+/**
+* Writes bytes to the stream.
+* @param stream The stream to write to.
+* @param strData The string to write to the stream.
+*/
+    private static void WriteBytesToStream(NetworkStream stream, string strData)
+    {
+        byte[] ASCIIBytes = Encoding.ASCII.GetBytes(strData);
+        
+        stream.Write(
+            ASCIIBytes,
+            0,
+            ASCIIBytes.Length
+        );
+    }
+}
